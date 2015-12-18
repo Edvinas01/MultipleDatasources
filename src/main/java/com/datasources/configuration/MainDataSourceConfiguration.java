7735cd4aab6fe2f171d138075e1e8cfe36b6939c
Edvinas Danevicius <edvinas.danevicius@getcujo.com>
2015-12-18 10:39:41 +0200

    Both profiles!!!

--- src/main/java/com/datasources/configuration/MainDataSourceConfiguration.java
+++ src/main/java/com/datasources/configuration/MainDataSourceConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -24,6 +25,7 @@ public class MainDataSourceConfiguration extends DataSourceConfiguration {
 
     @Bean
     @Primary
+    @Profile("default")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
@@ -32,8 +34,11 @@ public class MainDataSourceConfiguration extends DataSourceConfiguration {
     @Bean
     @Primary
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder) {
+
+        DataSource dataSource = dataSource();
+
         return builder
-                .dataSource(dataSource())
+                .dataSource(dataSource)
                 .packages("com.datasources.main.domain")
                 .persistenceUnit("default")
                 .properties(createProperties())
--- src/main/java/com/datasources/configuration/SecondaryDataSourceConfiguration.java
+++ src/main/java/com/datasources/configuration/SecondaryDataSourceConfiguration.java
@@ -1,11 +1,11 @@
 package com.datasources.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -23,33 +23,20 @@ import javax.sql.DataSource;
 )
 public class SecondaryDataSourceConfiguration extends DataSourceConfiguration {
 
-    @Value("${spring.secondaryDatasource.url}")
-    private String databaseUrl;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driver;
-
     @Bean
+    @Profile({"default", "secondary"})
+    @ConfigurationProperties("spring.dataSource")
     public DataSource secondaryDataSource() {
-        return DataSourceBuilder
-                .create()
-                .url(databaseUrl)
-                .username(username)
-                .password(password)
-                .driverClassName(driver)
-                .build();
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+
+        DataSource dataSource = secondaryDataSource();
+
         return builder
-                .dataSource(secondaryDataSource())
+                .dataSource(dataSource)
                 .packages("com.datasources.secondary.domain")
                 .persistenceUnit("secondary")
                 .properties(createProperties())
